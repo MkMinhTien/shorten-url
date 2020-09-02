9e7fba53f5022c1481d94dab3f3c56254c83dee5
MkMinhTien <60256636+MkMinhTien@users.noreply.github.com>
2020-09-02 16:06:53 +0700

    Update server.php

--- server.php
+++ server.php
@@ -3,9 +3,10 @@ if (isset($_GET['url']) && $_GET['url']) {
     include 'data/libs.php';
     $f = loaddata('data/urls.txt');
     $url = $f[$_GET['url']];
-    if ($url) {
-        header('location:' . $url['url']);
-    } else {
-        header('location:./');
-    }
+    header('location:' . $url['url']);
+    // if ($url) {
+    //     header('location:' . $url['url']);
+    // } else {
+    //     header('location:./');
+    // }
 }
